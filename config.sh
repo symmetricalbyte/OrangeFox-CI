44d6a09c1131f329f8d717ec37193eccda8061af
symmetricalbyte <73771649+symmetricalbyte@users.noreply.github.com>
2023-02-14 16:29:51 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/Redmi10C-AOSP/twrp_xiaomi_fog -b twrp-11"
+export FOX_BRANCH="fox_11.1"
+export DT_LINK="https://github.com/eun-Dumpfest/twrp_xiaomi_fog -b twrp-11"
 
 export DEVICE="fog"
 export OEM="xiaomi"
